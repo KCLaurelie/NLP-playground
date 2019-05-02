4b96f22d4e3dee9888358eab543c0786abe30870
aurelie <aurelie.mascio@kcl.ac.uk>
2019-05-02 20:25:51 +0100

    cleaned and added functions to save models

--- symptoms_classifier/NLP_utils.py
+++ symptoms_classifier/NLP_utils.py
@@ -4,30 +4,50 @@ from nltk.corpus import stopwords, words
 from nltk.stem.snowball import SnowballStemmer
 import json, pandas, re
 
+# save trained model
+def save_model_json(model,output_file):
+    # serialize model to JSON
+    model_json = model.to_json()
+    with open("model.json", "w") as json_file:
+        json_file.write(model_json)
+    # serialize weights to HDF5
+    model.save_weights("model.h5")
+    return 0
 
-#stemming_____________________________________________________________________________________________________________
+# load json and create model
+def load_model_json(json_file):
+    from keras.models import model_from_json
+    json_file = open(json_file, 'r')
+    loaded_model_json = json_file.read()
+    json_file.close()
+    loaded_model = model_from_json(loaded_model_json)
+    # load weights into new model & compile model
+    loaded_model.load_weights("model.h5")
+    loaded_model.compile(loss='binary_crossentropy',
+                         optimizer='adam',
+                         metrics=['accuracy'])
+    print("Loaded model from disk")
+
+    #score_saved_model = loaded_model.evaluate(x_test, y_test, verbose=0)
+    return loaded_model
+
+# stemming_____________________________________________________________________________________________________________
 stemmer = SnowballStemmer('english')
-
-
-#contraction__________________________________________________________________________________________________________
-contraction = json.load(open('constants/contraction.json'))
-
 
 # stopword_____________________________________________________________________________________________________________
 stop_words = stopwords.words('english')
 stop_words.remove('not')
 
-
-
 # correction__________________________________________________________________________________________________________
 def all_words(text):
     return re.findall('\\w+', text.lower())
 
-WORDS = Counter(all_words(open('constants/big.txt').read()))
+#word_dic = Counter(all_words(open('constants/big.txt').read()))
 
-def P(word, N=sum(WORDS.values())):
+def P(word, word_dic):
+    N=sum(word_dic.values())
     """Probability of `word`."""
-    return WORDS[word] / N
+    return word_dic[word] / N
 
 def correction(word):
     """Most probable spelling correction for word."""
@@ -37,9 +57,9 @@ def candidates(word):
     """Generate possible spelling corrections for word."""
     return known([word]) or known(edits1(word)) or known(edits2(word)) or [word]
 
-def known(words):
+def known(words, word_dic):
     """The subset of `words` that appear in the dictionary of WORDS."""
-    return set((w for w in words if w in WORDS))
+    return set((w for w in words if w in word_dic))
 
 def edits1(word):
     """All edits that are one edit away from `word`."""
@@ -57,31 +77,35 @@ def edits2(word):
 
 
 # word dictionary______________________________________________________________________________________________________
-word_dictionary = list(set(words.words()))
-for alphabet in 'bcdefghjklmnopqrstuvwxyz':
-    word_dictionary.remove(alphabet)
+def create_word_dictionary():
+    word_dictionary = list(set(words.words()))
+    for alphabet in 'bcdefghjklmnopqrstuvwxyz':
+        word_dictionary.remove(alphabet)
 
-useless_two_letter_words = pandas.read_csv('constants/useless_two_letter_words.csv')
-for word in useless_two_letter_words:
-    word_dictionary.remove(word)
+    useless_two_letter_words = pandas.read_csv('constants/useless_two_letter_words.csv')
+    for word in useless_two_letter_words:
+        word_dictionary.remove(word)
 
-useful_words = pandas.read_csv('constants/useful_words.csv')
-for word in useful_words:
-    word_dictionary.append(word)
+    useful_words = pandas.read_csv('constants/useful_words.csv')
+    for word in useful_words:
+        word_dictionary.append(word)
 
-for key in contraction:
-    word_dictionary.append(key)
+    contractions = pandas.read_csv('constants/contractions.csv')
+    for key in contractions:
+        word_dictionary.append(key)
+
+    return word_dictionary
 
 
 # split hashtags_______________________________________________________________________________________________________
-def split_hashtag(hashtag):
+def split_hashtag(hashtag, contractions, word_dictionary):
     found = False
     for i in reversed(range(1, len(hashtag) + 1)):
         if stemmer.stem(hashtag[:i]) in word_dictionary or hashtag[:i] in word_dictionary:
             found = True
             if i == len(hashtag):
-                if hashtag[:i] in contraction:
-                    contraction[hashtag[:i]]
+                if hashtag[:i] in contractions:
+                    contractions[hashtag[:i]]
                 else:
                     return hashtag[:i]
             else:
--- symptoms_classifier/symptoms_classifier.py
+++ symptoms_classifier/symptoms_classifier.py
@@ -1,5 +1,6 @@
+import os
 from imblearn.over_sampling import SMOTE
-from sklearn import naive_bayes, svm, tree, ensemble, linear_model
+from sklearn import naive_bayes, svm, tree, ensemble, linear_model, neighbors
 from sklearn.feature_extraction import text
 from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score
 from sklearn.model_selection import cross_val_predict
@@ -8,56 +9,59 @@ import json
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-
-test = False
-if len(argv) == 2 and argv[1] == 'test':
-    test = True
+os.chdir(r'C:\Users\K1774755\PycharmProjects\toy-models\symptoms_classifier')
 
 models = {
     'Multinomial NB': naive_bayes.MultinomialNB(),
+    'Gaussian NB': naive_bayes.GaussianNB(),
+    'KNN': neighbors.KNeighborsClassifier(n_neighbors=5, n_jobs=-1),
     'Decision Tree': tree.DecisionTreeClassifier(random_state=0),
-    'Random Forest': ensemble.RandomForestClassifier(criterion='entropy', n_jobs = 10),
+    'Random Forest': ensemble.RandomForestClassifier(criterion='entropy', n_jobs=10),
     'Logistic Regression': linear_model.LogisticRegression(),
+    'SVM': svm.SVC(),
+    'SVM with linear kernel': svm.SVC(kernel='linear'),
+    'SVM with sigmoid kernel': svm.SVC(kernel='sigmoid'),
+    'SVM with poly kernel': svm.SVC(kernel='poly')
 }
 
 
-def vectorizer(train_tweets, test_tweets=[]):
-    vectorizer = text.TfidfVectorizer(
-        min_df = 0.00125,
-        max_df = 0.7,
+def vectorizer(train_texts, test_texts=[]):
+    _vectorizer = text.TfidfVectorizer(
+        min_df=0.00125,
+        max_df=0.7,
         sublinear_tf=True,
         use_idf=True,
         analyzer='word',
-        ngram_range=(1,5)
+        ngram_range=(1, 5)
     )
-    train_vectors = vectorizer.fit_transform(train_tweets)
-    test_vectors = vectorizer.transform(test_tweets) if test_tweets else []
+    train_vectors = _vectorizer.fit_transform(train_texts)
+    test_vectors = _vectorizer.transform(test_texts) if test_texts else []
     return train_vectors, test_vectors
 
 
 def feature_generation(train_file, test_file=''):
-    with open(train_file,"r") as train_data_text:
+    with open(train_file, "r") as train_data_text:
         train_data = json.load(train_data_text)
-    train_tweets = []
+    train_texts = []
     train_class = []
-    for tweet in train_data:
-        train_tweets.append(tweet['text'])
-        train_class.append(tweet['label'])
+    for doc in train_data:
+        train_texts.append(doc['text'])
+        train_class.append(doc['label'])
 
-    test_tweets = []
+    test_texts = []
     if test_file:
-        with open(test_file,"r") as test_data_text:
+        with open(test_file, "r") as test_data_text:
             test_data = json.load(test_data_text)
-        for tweet in test_data:
-            test_tweets.append(tweet['text'])
+        for doc in test_data:
+            test_texts.append(doc['text'])
 
-    train_vectors, test_vectors = vectorizer(train_tweets, test_tweets)
+    train_vectors, test_vectors = vectorizer(train_texts, test_texts)
     return train_vectors, train_class, test_vectors
 
 
 def plot_distribution(class_array, title):
     plt.figure(title)
-    pd.DataFrame(class_array, columns = ['Class']).Class.value_counts().plot(
+    pd.DataFrame(class_array, columns=['Class']).Class.value_counts().plot(
         kind='pie',
         autopct='%.2f %%',
     )
@@ -74,22 +78,22 @@ def over_sample(train_vectors, train_class):
     return train_vectors, train_class
 
 
-def classify(classifier, train_vectors, train_class, test_vectors):
+def classify(classifier, train_vectors, train_class, test_vectors, test=False):
     if test:
         classifier.fit(train_vectors, train_class)
         preds = classifier.predict(test_vectors)
         return preds
     else:
         preds = cross_val_predict(classifier, train_vectors, train_class, cv=10)
-        accScore = accuracy_score(train_class,preds)
+        acc_score = accuracy_score(train_class,preds)
         labels = [1, -1]
-        precision = precision_score(train_class, preds, average=None,labels=labels)
-        recall = recall_score(train_class,preds,average=None,labels=labels)
-        f1score = f1_score(train_class,preds,average=None,labels=labels)
-        return accScore, precision, recall, f1score
+        precision = precision_score(train_class, preds, average=None, labels=labels)
+        recall = recall_score(train_class, preds, average=None, labels=labels)
+        f1score = f1_score(train_class, preds, average=None, labels=labels)
+        return acc_score, precision, recall, f1score
 
 
-def train_classify(train_file, test_file):
+def train_classify(train_file, test_file, test=False):
     train_vectors, train_class, test_vectors = feature_generation(train_file, test_file)
     plot_distribution(train_class, train_file + ' Before sampling')
     train_vectors, train_class = over_sample(train_vectors, train_class)
@@ -102,7 +106,7 @@ def train_classify(train_file, test_file):
             ('lr', models['Logistic Regression']),
         ], voting='soft')
         preds = classify(eclf, train_vectors, train_class, test_vectors)
-        f = open('data/' + candidate + '_predictions'  + '.txt', 'w+')
+        f = open('data/obama_predictions.text', 'w+')
         for index, pred in enumerate(preds):
             f.write(str(index+1)+';;'+str(preds[index])+'\n')
         f.close()
@@ -110,22 +114,23 @@ def train_classify(train_file, test_file):
         metrics = []
         for index, model in enumerate(models):
             print("Classifying using", model)
-            accScore, precision, recall, f1score = classify(models[model], train_vectors, train_class, test_vectors)
+            acc_score, precision, recall, f1_score = classify(models[model], train_vectors, train_class, test_vectors)
             metrics.append({})
             metrics[index]['Classifier'] = model
-            metrics[index]['accuracy'] = accScore
-            metrics[index]['possitive f1score'] = f1score[0]
-            metrics[index]['negative f1score'] = f1score[1]
+            metrics[index]['accuracy'] = acc_score
+            metrics[index]['possitive f1score'] = f1_score[0]
+            metrics[index]['negative f1score'] = f1_score[1]
         pd.io.json.json_normalize(metrics).plot(kind='bar', x='Classifier')
         plt.title(train_file)
         plt.grid(True, axis='y')
         plt.ylim(ymax=1)
         plt.xticks(rotation=0)
+        plt.show()
 
 
-for candidate in ['obama', 'romney']:
-    print('Running for', candidate, '...')
-    test_file = 'data/' + candidate + '_test_cleaned.json' if test else ''
-    train_classify('data/' + candidate + '_cleaned.json', test_file)
-
-plt.show()
+def testing():
+    for candidate in ['obama', 'romney']:
+        print('Running for', candidate, '...')
+        try: test_file = 'data/' + candidate + '_test_cleaned.json'
+        except: test_file = ''
+        train_classify('data/' + candidate + '_cleaned.json', test_file)
