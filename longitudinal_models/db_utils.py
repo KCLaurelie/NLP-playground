05f41c3ea0d3b1e8532d63673a8f0605eab08fcc
aurelie <aurelie.mascio@kcl.ac.uk>
2019-06-28 20:25:03 +0100

    added

---
+++ longitudinal_models/db_utils.py
@@ -0,0 +1,179 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 11 09:00:22 2019
+
+@author: AMascio
+"""
+
+import pandas as pd
+import pandas.io.sql as psql
+import pyodbc
+import sys
+import sqlalchemy as sa
+import os
+
+_server_name = 'BRHNSQL094'
+_db_name = 'SQLCRIS'
+root_path=r'T:\aurelie_mascio'
+CRIS_data_path=root_path+'\\CRIS data'
+SQL_path=root_path+'\\SQL queries'
+
+def connect(server_name=_server_name, db_name=_db_name):
+  connection_string = 'Driver={SQL Server Native Client 10.0};Server=' + server_name + ';Database=' + db_name + ';Trusted_Connection=yes;'
+  connection = pyodbc.connect(connection_string)
+  return connection
+
+def query_sql_alchemy(query,server_name=_server_name, db_name=_db_name):
+    connection_string = 'Driver={SQL Server Native Client 10.0};Server=' + server_name + ';Database=' + db_name + ';Trusted_Connection=yes;'
+    engine=sa.create_engine("mssql+pyodbc:///?odbc_connect={}".format(connection_string))
+    
+    if '.sql' in query:
+        fd = open(query, 'r')
+        sqlFile = fd.read()
+        fd.close()
+        sqlCommand = sqlFile.split(';')[0] #only take 1st query
+    else:
+        sqlCommand = query
+    #query="select top 10 * from [SQLCRIS].[dbo].Attachment"
+    sql_reader=pd.read_sql(sqlCommand,con=engine,chunksize=10000)
+    res=pd.Dataframe()
+    for sql_chunk in sql_reader:
+        res=pd.concat([res,sql_chunk])
+        
+    return res
+
+def list_table_names(server_name=_server_name, db_name=_db_name):
+  query = """SELECT TABLE_NAME
+  FROM INFORMATION_SCHEMA.TABLES
+  WHERE TABLE_TYPE = 'BASE TABLE' AND TABLE_CATALOG='{}'""".format(db_name)
+
+  connection = connect(server_name, db_name)
+  df = psql.read_sql(query, connection)
+  connection.close()
+
+  return df['TABLE_NAME'].tolist()
+
+
+def list_column_names(table_name, server_name=_server_name, db_name=_db_name, verbose=False):
+  query = """SELECT *
+  FROM INFORMATION_SCHEMA.COLUMNS
+  WHERE TABLE_NAME = '{}'
+  AND TABLE_SCHEMA='dbo'""".format(table_name)
+
+  query = """SELECT TABLE_NAME, COLUMN_NAME, DATA_TYPE, CHARACTER_MAXIMUM_LENGTH, ORDINAL_POSITION
+  FROM INFORMATION_SCHEMA.COLUMNS
+  WHERE TABLE_NAME = '{}'
+  ORDER BY TABLE_NAME, ORDINAL_POSITION""".format(table_name)
+
+  if verbose:
+      print('Query:', query)
+
+  connection = connect(server_name=_server_name, db_name=_db_name)
+  df = psql.read_sql(query, connection)
+  connection.close()
+
+  return df['COLUMN_NAME'].tolist()
+
+
+def fetch_dataframe(query,server_name=_server_name, db_name=_db_name):
+  print('-- Fetching DataFrame. Server:', server_name, 'Database:', db_name, file=sys.stderr)
+  df = pd.DataFrame()
+
+  try:
+      connection = connect(server_name=_server_name, db_name=_db_name)
+      df = psql.read_sql(query, connection)
+      connection.close()
+  except Exception as e:
+      print("-- Error accessing database", file=sys.stderr)
+      print(str(e), file=sys.stderr)
+
+  return df
+
+#extract_corpus_for_brcid_list(sql_query=all_attachments_sql,output_path=root_path+r'\multimorbidity\corpus', brcid_source=root_path+r'\multimorbidity\brcid_list.csv')
+def extract_corpus_for_brcid_list(sql_query,
+                                  server_name=_server_name, db_name=_db_name,
+                                  output_path=root_path+r'\F20 corpus\training',
+                                  brcid_source=CRIS_data_path+r'\F20_brcid_training.csv',
+                                  start_date='2007-01-01'):
+    all_brcids=pd.read_csv(brcid_source,header=0,chunksize=2000,usecols=['brcid'])
+    connection = connect(server_name=_server_name, db_name=_db_name)
+    for chunk in all_brcids:
+        tmp_brc_ids=",".join(map(str,list(chunk.brcid.unique())))
+#        formatted_sql_query = """ select distinct convert(varchar,att.Date,23) as 'Date',CN_Doc_ID, BrcId,Attachment_Text
+#        from [SQLCRIS].[dbo].Attachment att
+#        where BrcId in ({})
+#        and Attachment_Text is not null and Attachment_Text!='' and DATALENGTH( Attachment_Text)>1
+#        """.format(tmp_brc_ids)
+        formatted_sql_query=sql_query.format(start_date,tmp_brc_ids,tmp_brc_ids,tmp_brc_ids,tmp_brc_ids)
+        query_res = psql.read_sql(formatted_sql_query, connection)
+        for record in query_res.itertuples():
+            if len(record.Attachment_Text)>1: # sometimes sql query doesn't exclude all empty texts...
+                print('generating corpus for patient',record.BrcId)
+                title=str(record.BrcId)+'_'+str(record.Date)+'_'+str(record.CN_Doc_ID)
+                tmp_path=output_path+'\\'+str(record.BrcId)
+                if not os.path.exists(tmp_path): os.makedirs(tmp_path)
+                with open(os.path.join(tmp_path,title+'.txt'), "w") as text_file:
+                    try: text_file.write(record.Attachment_Text) #sometimes it breaks because of weird characters
+                    except: pass #in which case we skip for now (TODO: clean weird stuff in SQL query)
+    connection.close()
+    return 0
+
+def execute_sql(SQL_file,
+                server_name=_server_name,
+                db_name=_db_name,
+                save_to_csv=False):
+    res=pd.DataFrame()
+    fd = open(SQL_file, 'r')
+    sqlFile = fd.read()
+    fd.close()
+
+    # all SQL commands (split on ';')
+    sqlCommands = sqlFile.split(';')
+    
+    # Execute every command from the input file
+    for command in sqlCommands:
+        res=pd.concat([res,fetch_dataframe(server_name,db_name,str(command))])
+    if save_to_csv:
+        res.to_csv(SQL_file.replace('.sql','.csv'))
+        return 'file saved under: ' + SQL_file.replace('.sql','.csv')
+    return res
+
+#test=execute_sql(SQL_file=SQL_path+'\\F20_all_texts.sql')
+#test=test.sort_values(by=['BrcId', 'date'])
+#test.to_csv(SQL_path+'\\F20_all_texts.sql')
+    
+all_attachments_sql="""
+DECLARE @min_diagnosis_date DATETIME='{}';
+WITH attachments as 
+(
+select distinct
+  att.BrcId, cast(att.CN_Doc_ID as bigint) as CN_Doc_ID, convert(varchar,att.Date,23) as doc_date, att.Attachment_Text, 'attachment' as doc_type
+  FROM SQLCRIS.dbo.Attachment att 
+  where DATALENGTH(att.Attachment_Text)>0 and att.Date > DATEADD(month,-6,@min_diagnosis_date)
+  and BrcId in ({})
+ )
+,discharge_docs as 
+(
+select distinct dc.BrcId, cast(dc.CN_Doc_ID as bigint) as CN_Doc_ID, convert(varchar,dc.Completion_Date,23) as doc_date, dc.Brief_Summary as Attachment_Text, 'discharge_summary' as doc_type
+    from SQLCRIS.dbo.Discharge_Notification_Summary dc 
+    where DATALENGTH(dc.Brief_Summary)>0 and dc.Completion_Date > DATEADD(month,-6,@min_diagnosis_date)
+    and BrcId in ({})
+) 
+,discharge_plans as 
+(
+select distinct dc.BrcId, cast(dc.CN_Doc_ID as bigint) as CN_Doc_ID, convert(varchar,dc.Completion_Date,23) as doc_date, dc.Discharge_Plan as Attachment_Text, 'discharge_plan' as doc_type
+    from SQLCRIS.dbo.Discharge_Notification_Summary dc 
+    where DATALENGTH(dc.Brief_Summary)>0 and dc.Completion_Date > DATEADD(month,-6,@min_diagnosis_date)
+    and BrcId in ({})
+)
+,correspondences as 
+(
+select distinct cc.BrcId, cast(cc.CN_Doc_ID as bigint) as CN_Doc_ID, convert(varchar,cc.Date,23) as doc_date,cc.Attachment_Text, 'correspondence' as doc_type
+    from SQLCRIS.dbo.CCS_Correspondence cc 
+    where DATALENGTH(cc.Attachment_Text)>0 and cc.Date > DATEADD(month,-6,@min_diagnosis_date)
+    and BrcId in ({})
+) 
+
+select * from  attachments UNION select * from discharge_docs union select * from discharge_plans union select * from correspondences
+
+"""
---
+++ longitudinal_models/general_utils.py
@@ -0,0 +1,264 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr  1 10:46:34 2019
+
+@author: AMascio
+"""
+import os
+import pandas as pd
+import numpy as np
+import datetime
+import sys
+import csv
+from collections import OrderedDict
+import pyximport
+
+pyximport.install()
+sys.maxsize
+csv.field_size_limit(200000000)
+
+root_path=r'T:\aurelie_mascio'
+CRIS_data_path=root_path+'\\CRIS data'
+SQL_path=root_path+'\\SQL queries'
+os.chdir(root_path+r'\python_scripts') #directory with python library
+headers_dict_file=root_path+r'\python_scripts\CRIS_data_dict.csv'
+patients_data_file=CRIS_data_path+r'\F20_patients_documents_details_from_DB.csv'
+
+##############################################################################
+## GENERAL UTILS FUNCTIONS
+############################################################################## 
+# standardize dataframe data
+def clean_df(df,to_numeric=True,filter_col=None,filter_value=None, threshold_col=None, threshold_value=None):
+    df.columns = df.columns.str.lower()
+    df=df.rename(columns={'brc_id': 'brcid'})
+    if 'brcid' in df.columns: df.dropna(subset=['brcid'],inplace=True)
+    date_cols=[x for x in df.columns if 'date' in x]
+    df[date_cols]=df[date_cols].apply(pd.to_datetime, errors='ignore')
+    if to_numeric:
+        non_date_cols=[col for col in df.columns if df[col].dtype != 'datetime64[ns]']
+        df[non_date_cols]=df[non_date_cols].apply(pd.to_numeric,errors='ignore')
+    if filter_col is not None: df = df[df[filter_col].isin(filter_value)]
+    if threshold_col is not None: df = df.loc[df[threshold_col]>=threshold_value]
+    df.drop_duplicates(inplace=True)
+    return df
+
+def getDuplicateColumns(df):
+    '''
+    Get a list of duplicate columns.
+    It will iterate over all the columns in dataframe and find the columns whose contents are duplicate.
+    :param df: Dataframe object
+    :return: List of columns whose contents are duplicates.
+    '''
+    duplicateColumnNames = set()
+    # Iterate over all the columns in dataframe
+    for x in range(df.shape[1]):
+        # Select column at xth index.
+        col = df.iloc[:, x]
+        # Iterate over all the columns in DataFrame from (x+1)th index till end
+        for y in range(x + 1, df.shape[1]):
+            # Select column at yth index.
+            otherCol = df.iloc[:, y]
+            # Check if two columns at x 7 y index are equal
+            if col.equals(otherCol):
+                duplicateColumnNames.add(df.columns.values[y])
+ 
+    return list(duplicateColumnNames)
+
+def drop_duplicate_columns(df):
+    new_df = df.drop(columns=getDuplicateColumns(df))
+    return new_df
+
+## COUNT NUMBER WORDS IN TEXT FILE
+def count_words(file_name):
+    num_words = 0
+    with open(file_name, 'r') as f:
+        for line in f:
+            words = line.split()
+            num_words += len(words)
+    return num_words
+
+
+## read csv file 
+# file_path=r'T:\aurelie_mascio\multimorbidity\corpus\multimorbidity_attachments_discharge_docs.csv'
+def super_read_csv(file_path,usecols=None,clean_results=True, filter_col=None,filter_value=None, threshold_col=None, threshold_value=None,
+                   read_from_SQL_export=False):
+    #f = open(csv_file, encoding='utf-8-sig')
+    # df = pd.read_csv(csv_file, sep='|', header=0, encoding='cp1252', quoting=1, escapechar='\\', engine='python')
+            
+    if os.stat(file_path).st_size/1e9>=1.5: # large csv files need to be read by chunks
+        # 1. READ THE FILE IN CHUNKS
+        if read_from_SQL_export: # file exported using SSIS package method
+            TextFileReader = pd.read_csv(file_path, sep='|', header=0, encoding='ansi', quoting=1, escapechar='\\', engine='python', usecols=usecols, chunksize=10000)
+        else:
+            TextFileReader=pd.read_csv(file_path,usecols=usecols,chunksize=10000,header=0,engine='python',error_bad_lines=False)
+            #TextFileReader=pd.read_csv(file_path,usecols=usecols,chunksize=10000,low_memory=False,header=0,encoding='utf8',engine='c',error_bad_lines=False)
+        #test=TextFileReader.get_chunk(3)
+        #row_count = sum(1 for row in TextFileReader)
+        
+        # 2. RE-ASSEMBLE THE FILE CHUNKS IN A DATAFRAME
+        try: #if we're lucky we can load all that in 1 go
+            res = pd.concat(TextFileReader, ignore_index=True)
+            if clean_results: res=clean_df(res,filter_col=filter_col,filter_value=filter_value, threshold_col=threshold_col, threshold_value=threshold_value)
+        except: #if we're less lucky let's concatenate each chunk individually
+            res=pd.DataFrame()
+            for chunk in TextFileReader:
+                if clean_results: chunk=clean_df(chunk,filter_col=filter_col,filter_value=filter_value, threshold_col=threshold_col, threshold_value=threshold_value)
+                res=pd.concat([res, chunk],ignore_index=True)
+                
+    else: #normal sized csv files
+        if read_from_SQL_export: # file exported using SSIS package method
+            res = pd.read_csv(file_path, sep='|', header=0, encoding='ansi', quoting=1, escapechar='\\', engine='python', usecols=usecols)
+        else:
+            try: #file exported directly from SQL (not using SSIS export method)
+                res=pd.read_csv(file_path,header=0,usecols=usecols,engine='python',error_bad_lines=False)
+            except: #file was likely created from excel
+                res=pd.read_csv(file_path,header=0,usecols=usecols,engine='python',error_bad_lines=False,encoding='ISO-8859-1')
+        if clean_results: res=clean_df(res,filter_col=filter_col,filter_value=filter_value, threshold_col=threshold_col, threshold_value=threshold_value)
+    return res
+
+## LOAD SYMPTOMS APP RESULTS (headers specific to CRIS NLP apps)
+def load_symptoms_df(symptoms_file,
+                     obs_filter=['positive'],
+                     confidence_threshold=0.5,
+                     usecols=['BrcID','observation','Document_Date','confidence']):
+    res_df=pd.DataFrame()
+    TextFileReader=pd.read_csv(symptoms_file,usecols=usecols,chunksize=10000,header=0,engine='python',error_bad_lines=False)
+    for chunk in TextFileReader:
+        if len(obs_filter)>0: chunk = chunk[chunk.observation.isin(obs_filter)]
+        res_df=pd.concat([res_df, chunk.loc[chunk.confidence>confidence_threshold]], ignore_index=True)
+    res_df= clean_df(res_df)
+    res_df['year']=res_df.document_date.dt.year
+    return res_df
+
+##############################################################################
+## RANDOM STUFF
+##############################################################################
+
+def cut_with_na(to_bin, bins, labels, na_category = 'not known'):
+    to_bin = pd.to_numeric(to_bin, errors='coerce')
+    res = pd.cut(pd.Series(to_bin),
+                 bins=bins,
+                 labels=labels
+                 ).values.add_categories(na_category)
+    res = res.fillna(na_category)
+    return res
+    
+# source: https://www.nhs.uk/common-health-questions/lifestyle/what-is-the-body-mass-index-bmi/    
+def bmi_category(score, na_category = 'not known'):
+    res = cut_with_na(to_bin=score,
+                      bins=[-np.inf, 18.5, 25, 30, np.inf],
+                      labels=['underweight', 'normal','preobese','obese'],
+                      na_category=na_category)
+    return res
+
+# source: https://www.cdc.gov/bloodpressure/measure.htm
+def blood_pressure(systolic, diastolic, na_category = 'not known'):
+    ratio = pd.to_numeric(systolic, errors='coerce') / pd.to_numeric(diastolic, errors='coerce')
+    res = cut_with_na(to_bin=ratio,
+                      bins=[-np.inf, 120./80, 140./90, np.inf],
+                      labels=['normal','prehypertension','hypertension'],
+                      na_category=na_category)
+    return res
+
+# source: https://www.diabetes.co.uk/fasting-plasma-glucose-test.html        
+def diabetes(plasma_glucose, na_category = 'not known'):
+    res = cut_with_na(to_bin=plasma_glucose,
+                      bins=[-np.inf, 5.5, 7, np.inf],
+                      labels=['normal','prediabetic','diabetic'],
+                      na_category=na_category)
+    return res
+
+def convert_num_to_bucket(nb, bucket_size=0.5, convert_to_str=True):
+    lower_bound = np.ceil(nb / bucket_size)*bucket_size - bucket_size
+    upper_bound = np.ceil(nb / bucket_size)*bucket_size
+    res = [lower_bound, upper_bound]
+    if convert_to_str: res = str(res)
+    return res
+
+##############################################################################
+## DATE FUNCTIONS FOR LONGITUDINAL MODELLING
+############################################################################## 
+#dates = ["2014-10-10", "2016-01-07"]
+
+def monthlist_short(dates):
+    start, end = [datetime.strptime(_, "%Y-%m-%d") for _ in dates]
+    return OrderedDict(((start + datetime.timedelta(_)).strftime(r"%b-%y"), None) for _ in range((end - start).days)).keys()
+
+def monthlist_fast(dates):
+    start, end = [datetime.strptime(_, "%Y-%m-%d") for _ in dates]
+    total_months = lambda dt: dt.month + 12 * dt.year
+    mlist = []
+    for tot_m in range(total_months(start)-1, total_months(end)):
+        y, m = divmod(tot_m, 12)
+        mlist.append(datetime(y, m+1, 1).strftime("%b-%y"))
+    return mlist
+
+##############################################################################
+## GENERATE LONGITUDINAL DATA FROM RAW DATA (HOSPITAL STAYS, MEASURES...)
+##############################################################################
+#test_file=r'T:\aurelie_mascio\CRIS data\F20_ward_stays_data_fromDB.csv'
+#patients_data=r'T:\aurelie_mascio\CRIS data\F20_patients_details_from_DB.csv'
+#test_df=pd.read_csv(test_file,header=0)
+#df=test_df.head().copy()
+#df['dob']=datetime.datetime(1986, month=8, day=13)
+
+## FUNCTION TO GENERATE LONGITUDINAL DATA BY AGE
+def convert_to_longitudinal_age(df,
+                                key_col='brcid',
+                                dob_col='dob',
+                                start_date_col='actual_start_date', #for interval only
+                                end_date_col='actual_end_date', #for interval only
+                                measure_date_col='rating_date', #for rating only
+                                measure_col='length_stay_days', #for rating only
+                                dob_file=patients_data_file,
+                                mode='interval'): #interval: need to measure length (e.g. length of hospital stay), not interval: need to grab measure (e.g. HoNOS) by age
+    df=clean_df(df)
+    dob_data=pd.read_csv(dob_file,header=0)
+    #######################################
+    ### TODO: JOIN WITH DOB DATA
+    #######################################
+    if mode!='interval': # we just want to aggregate 1 measure by age, taking the average
+        df['rating_age']=round((df[measure_date_col]-df[dob_col]).dt.days/365,0)
+        res = pd.pivot_table(df, values=measure_col, index=[key_col],columns=['rating_age'], aggfunc=np.mean)
+        #res.unstack().reset_index()
+        return res
+    df['start_age']=(df[start_date_col]-df[dob_col]).dt.days/365
+    df['end_age']=(df[end_date_col]-df[dob_col]).dt.days/365
+    min_age=np.floor(df.start_age.min())
+    max_age=np.ceil(df.end_age.max())
+    for age in range(int(min_age),int(max_age)):
+        up_bound=np.minimum(age+1,df.end_age)
+        low_bound=np.maximum(age,df.start_age)
+        df[str(age)]=np.maximum(0,up_bound-low_bound)*365
+    
+    res_cols=[key_col]+list(map(str,range(int(min_age),int(max_age))))
+    res=df[res_cols].groupby([key_col],as_index=False).sum().sort_values(by=[key_col])
+    return res
+
+## FUNCTION TO GENERATE LONGITUDINAL DATA BY YEAR
+def convert_to_longitudinal_dates(df,
+                                  key_col='brcid',
+                                  start_date_col='actual_start_date',
+                                  end_date_col='actual_end_date',
+                                  measure_date_col='rating_date',
+                                  measure_col='length_stay_days',
+                                  mode='interval'):
+    df=clean_df(df)
+    if mode!='interval': # we just want to aggregate 1 measure by age, taking the average
+        df['rating_year']=df[measure_date_col].dt.year
+        res = pd.pivot_table(df, values=measure_col, index=[key_col],columns=['rating_year'], aggfunc=np.mean)
+        return res
+
+    min_date=df[start_date_col].dt.year.min()
+    max_date=df[end_date_col].dt.year.max()
+    for year in range(int(min_date),int(max_date+1)):
+        interval_start_date = datetime.datetime(year, month=1, day=1)
+        interval_end_date = datetime.datetime(year+1, month=1, day=1)
+        up_bound=np.minimum(pd.Series(len(df)*[interval_end_date]),df.actual_end_date)
+        low_bound=np.maximum(pd.Series(len(df)*[interval_start_date]),df.actual_start_date)
+        df[str(year)]=np.maximum(0,(up_bound-low_bound).dt.days)
+    
+    res_cols=[key_col]+list(map(str,range(int(min_date),int(max_date+1)))) #[x for x in df.columns if 'stay' in x or key_col in x]
+    res=df[res_cols].groupby([key_col],as_index=False).sum().sort_values(by=[key_col])
+    return res
+
