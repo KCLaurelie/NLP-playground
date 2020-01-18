44177eeb770d5f6e549481f4d4e61e7b8a4d53f3
aurelie <aurelie.mascio@kcl.ac.uk>
2020-01-18 11:56:14 +0000

    added

---
+++ longitudinal_models/data_stats.py
@@ -0,0 +1,73 @@
+import pandas as pd
+import numpy as np
+from scipy import stats
+from scipy.stats import mannwhitneyu
+
+data_file = r'C:\Users\K1774755\Downloads\phd\mmse_rebecca\mmse_synthetic_data_20190919.xlsx'
+subgroup_cols = ['age_bucket_baseline', 'gender', 'ethnicity_group2', 'marital_status', 'education', 'first_language',
+                 'imd_bucket_baseline2', 'smoking_status_baseline', 'cvd_problem']
+df = pd.read_excel(data_file, sheet_name='combined', index_col=None)
+df = df.loc[(df.age_at_first_diag > 49.5) & (df.age_at_score_baseline >= 50)]
+
+
+def ttest(df, value='score_combined',
+          group_col='patient_diagnosis_super_class',
+          groups_to_study=['organic only', 'smi+organic'],
+          subgroup_col='age_bucket_baseline'):
+    usecols = [subgroup_col, group_col, value] if subgroup_col is not None else [group_col, value]
+    #df = pd.read_excel(data_file, sheet_name='combined', index_col=None, usecols=usecols)
+    subgroups = df[subgroup_col].unique() if subgroup_col is not None else ['whatever']
+    subgroups.sort()
+
+    res = pd.DataFrame(columns=['t (ttest)', 'p (ttest)', 'stat (utest)', 'p (utest)'])
+    for sub in subgroups:
+        df_tmp = df.loc[df[subgroup_col] == sub] if subgroup_col is not None else df
+        g1 = df_tmp.loc[df_tmp[group_col] == groups_to_study[0], value]
+        g2 = df_tmp.loc[df_tmp[group_col] == groups_to_study[1], value]
+        t, p = stats.ttest_ind(g1, g2, equal_var=False)
+        stat, pu = mannwhitneyu(g1, g2)
+        res.loc[sub] = [t, p, stat, pu]
+        print('variable=', sub, 't (ttest)=', t, 'p (ttest)=', p)
+    return res
+
+
+def chisq(df, value='brcid',
+          group_col='patient_diagnosis_super_class',
+          groups_to_study=['organic only', 'smi+organic']):
+    usecols = subgroup_cols + [group_col, value]
+    #df = pd.read_excel(data_file, sheet_name='combined', index_col=None, usecols=usecols)
+    df = df[df[group_col].isin(groups_to_study)]
+
+    res = pd.DataFrame()  # (columns=['g', 'p'])
+    for sub in subgroup_cols:
+        df_tmp = df.pivot_table(values=value, index=sub, columns=group_col, aggfunc=pd.Series.nunique, margins=False,
+                                fill_value=0)
+        chi2, p, dof, ex = stats.chi2_contingency(df_tmp)
+        df_tmp['variable'] = sub
+        df_tmp['chi2'] = chi2
+        df_tmp['p'] = p
+        res = pd.concat([res, df_tmp])
+    return res
+
+
+def data_stats(df, group_col='patient_diagnosis_super_class'):
+    #df = pd.read_excel(data_file, sheet_name='combined', index_col=None)
+
+    res = pd.DataFrame()
+    for sub in subgroup_cols:
+        df_countunique = df.pivot_table(values='brcid', index=sub, columns=group_col, aggfunc=pd.Series.nunique, margins=True, fill_value=0)
+        df_countunique.columns = [x + '(1. N)' for x in df_countunique.columns]
+        for col in df_countunique.columns:
+            df_countunique[col.replace('N', '%')] = (df_countunique[col] / (df_countunique[col].sum()/2) * 100)
+        df_count = df.pivot_table(values='brcid', index=sub, columns=group_col, aggfunc='count', margins=True, fill_value=0)
+        df_count.columns = [x + '(2. obs)' for x in df_count.columns]
+        df_mean = df.pivot_table(values='score_combined', index=sub, columns=group_col, aggfunc=np.mean, margins=True)
+        df_mean.columns = [x + '(3. M)' for x in df_mean.columns]
+        df_sd = df.pivot_table(values='score_combined', index=sub, columns=group_col, aggfunc=np.std, margins=True)
+        df_sd.columns = [x + '(4. SD)' for x in df_sd.columns]
+        res_tmp = pd.concat([df_mean, df_count, df_sd, df_countunique], axis=1)
+        res_tmp = res_tmp.reindex(sorted(res_tmp.columns), axis=1)
+        res_tmp['variable'] = sub
+        res = pd.concat([res, res_tmp])
+
+    return res
