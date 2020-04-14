c7d8db497081db86c05ed865fabc9ce117b5d33d
aurelie <aurelie.mascio@kcl.ac.uk>
2020-04-14 12:42:12 +0100

    added input columns as input

--- longitudinal_models/imputation.py
+++ longitudinal_models/imputation.py
@@ -68,9 +68,10 @@ def impute_all_data(df, output_column=None, input_columns_to_exclude=None, clean
 def impute_with_baseline(df,
                          key='brcid',
                          baseline_cols=['brcid', 'score_date_upbound'],
+                         output_column=None,
                          input_columns_to_exclude=['brcid', 'score_combined', 'occur', 'counter', 'score_combined_baseline']):
     df_baseline = df.sort_values(baseline_cols).groupby(key).first().reset_index()
-    df_imputed = impute_all_data(df_baseline, output_column=None, clean_df=True,
+    df_imputed = impute_all_data(df_baseline, output_column=output_column, clean_df=True,
                                  input_columns_to_exclude=input_columns_to_exclude)
     df = df.merge(df_imputed[[key] + [x for x in df_imputed.columns if '_final' in x]], on=key)
     df = df.reindex(sorted(df_imputed.columns), axis=1)
