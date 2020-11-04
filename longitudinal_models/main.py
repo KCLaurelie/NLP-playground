9453664c4e2c0f52d1f340554f71122ae8a86793
aurelie <aurelie.mascio@kcl.ac.uk>
2020-11-04 14:44:24 +0000

    more tests for resubmission

--- longitudinal_models/main.py
+++ longitudinal_models/main.py
@@ -6,39 +6,58 @@ import statsmodels.api as sm
 import statsmodels.formula.api as smf
 import statsmodels.regression.mixed_linear_model as mlm
 
-df = pd.read_excel(r'C:\Users\K1774755\Downloads\phd\mmse_rebecca\mmse_trajectory_data_20200407_synthetic.xlsx', index_col=None, sheet_name='combined')
-df = df.loc[df.patient_diagnosis_super_class != 'SMI only']
-#df = df.loc[df.include == 'yes']
+df = pd.read_excel(r'C:\Users\K1774755\Downloads\phd\mmse_rebecca\mmse_trajectory_data_20200407_synthetic.xlsx',
+                   index_col=None, sheet_name='combined')
+df = df.loc[(df.patient_diagnosis_super_class != 'organic only')]
+#df = df.loc[df.duration !=False ,['brcid','duration','patient_diagnosis_super_class']]
+#res=ttest_ind(df, value='score_combined', group_col='patient_diagnosis_super_class')
+#df = df.loc[(df.patient_diagnosis_super_class != 'SMI only') & (df.age_at_dementia_diag >= 50)]
 
-def test(df, impute=False, bucket_data=False, calc_stats=False):
+def test(df, impute=False, bucket_data=False, calc_stats=False, complete=False):
 
     if bucket_data:
-        df_bucket = gutils.bucket_data(df, to_bucket='age_at_score', key='brcid', bucket_min=50, bucket_max=90, interval=0.5
+        df = gutils.bucket_data(df, to_bucket='age_at_score', key='brcid', bucket_min=50, bucket_max=90, interval=0.5
                                        , cols_to_exclude=None, na_values='unknown', min_obs=3, timestamp_cols=['age_at_score', 'score_date'])
     if impute:  # if imputed data needs to be generated (this step takes a while)
-        df_imp = impute_with_baseline(df_bucket, key='brcid', baseline_cols=['brcid', 'score_date'],
+        df = impute_with_baseline(df, key='brcid', baseline_cols=['brcid', 'score_date'],
                                       output_column=['gender', 'ethnicity', 'first_language', 'marital_status', 'education', 'imd_bucket_baseline', 'smoking_status_baseline', 'cvd_problem_baseline'],
                                       input_columns_to_exclude=['brcid', 'age_at_dementia_diag', 'occur', 'counter', 'score_date_centered', 'score_date', 'age_at_score_centered', 'age_at_score_baseline'])
     if calc_stats:  # NEED TO DO ON RAW DATA
-        stats0 = data_stats(df)
-        stats1 = ttest(df, groups_to_study=['organic only', 'SMI+organic'])
-        stats2 = chisq(df, groups_to_study=['organic only', 'SMI+organic'])
+        stats0 = data_stats(df, group_col='patient_diagnosis_super_class')
+        stats1 = ttest(df, group_col='patient_diagnosis_super_class', groups_to_study=['organic only', 'SMI+organic'])
+        stats2 = chisq(df, group_col='patient_diagnosis_super_class', groups_to_study=['organic only', 'SMI+organic'])
+    if complete:
+        df = df.replace({'unknown': np.nan, 'not known': np.nan}).dropna()
 
     socio_dem_imp = ['gender', 'ethnicity_imputed', 'marital_status_imputed', 'education',
-                     'first_language_imputed', 'imd_bucket_baseline_imputed', 'age_at_dementia_diag']
+                     'first_language_imputed', 'imd_bucket_baseline_imputed'] #, 'age_at_dementia_diag'
     cvd_imp = ['smoking_status_baseline_imputed', 'cvd_problem_baseline_imputed']
-    med = ['dementia_medication_baseline', 'antipsychotic_medication_baseline','antidepressant_medication_baseline']
+    med = ['dementia_medication_baseline', 'antipsychotic_medication_baseline', 'antidepressant_medication_baseline']
     references = {'education': 'no', 'ethnicity_imputed': 'white', 'ethnicity': 'white', 'smoking_status_baseline_imputed': 'no',
                   'smoking_status_baseline': 'no', 'marital_status_imputed': 'married_cohabitating',
-                  'marital_status': 'married_cohabitating'}
+                  'marital_status': 'married_cohabitating', 'patient_diagnosis_class':'organic only'}
     socio_dem = [x.replace('_imputed', '') for x in socio_dem_imp]
     cvd = [x.replace('_imputed', '') for x in cvd_imp]
 
     for key, val in references.items():
         if key in df: df[key] = df[key].replace({val: 'aaa_' + val})
 
-    covariates = ['age_at_score_baseline', 'patient_diagnosis_super_class'] + socio_dem_imp + cvd_imp + med
-    res = run_models(timestamps=('score_date_centered',), model_data=df, models=('linear_rdn_all',), covariates=covariates, covariates_slope=True, patients_split_col=None)
+    covariates = ['patient_diagnosis_class', 'age_at_score_baseline']
+    res0 = run_models(timestamps=('score_date_centered',), model_data=df, models=('linear_rdn_all',), covariates=covariates, covariates_slope=True, patients_split_col=None)
+    covariates = ['patient_diagnosis_class', 'age_at_score_baseline'] + socio_dem
+    res1 = run_models(timestamps=('score_date_centered',), model_data=df, models=('linear_rdn_all',), covariates=covariates, covariates_slope=True, patients_split_col=None)
+    covariates = ['patient_diagnosis_class', 'age_at_score_baseline'] + socio_dem + ['smoking_status_baseline']
+    res2 = run_models(timestamps=('score_date_centered',), model_data=df, models=('linear_rdn_all',), covariates=covariates, covariates_slope=True, patients_split_col=None)
+    covariates = ['patient_diagnosis_class', 'age_at_score_baseline'] + socio_dem + cvd
+    res3 = run_models(timestamps=('score_date_centered',), model_data=df, models=('linear_rdn_all',), covariates=covariates, covariates_slope=True, patients_split_col=None)
+    covariates = ['patient_diagnosis_class', 'age_at_score_baseline'] + socio_dem + cvd + med
+    res4 = run_models(timestamps=('score_date_centered',), model_data=df, models=('linear_rdn_all',), covariates=covariates, covariates_slope=True, patients_split_col=None)
+    covariates = ['patient_diagnosis_class', 'age_at_score_baseline'] + socio_dem + cvd + ['dementia_medication_baseline']
+    res5 = run_models(timestamps=('score_date_centered',), model_data=df, models=('linear_rdn_all',), covariates=covariates, covariates_slope=True, patients_split_col=None)
+    covariates = ['patient_diagnosis_class', 'age_at_score_baseline'] + socio_dem + cvd + ['antipsychotic_medication_baseline']
+    res6 = run_models(timestamps=('score_date_centered',), model_data=df, models=('linear_rdn_all',), covariates=covariates, covariates_slope=True, patients_split_col=None)
+    covariates = ['patient_diagnosis_class', 'age_at_score_baseline'] + socio_dem + cvd + ['antidepressant_medication_baseline']
+    res7 = run_models(timestamps=('score_date_centered',), model_data=df, models=('linear_rdn_all',), covariates=covariates, covariates_slope=True, patients_split_col=None)
 
     # models = ('linear_rdn_all', 'linear_rdn_int', 'quadratic_rdn_int')
     # covariates_slope = True
