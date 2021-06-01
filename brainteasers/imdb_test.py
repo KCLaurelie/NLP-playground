bfaa905117cbc36e51d9d169a0fbb260eb60a068
KCLaurelie <aurelie.mascio@kcl.ac.uk>
2021-06-01 17:38:06 +0100

    cleanup

--- brainteasers/imdb_test.py
+++ brainteasers/imdb_test.py
@@ -3,8 +3,7 @@ QUESTION 1
 """
 flightDuration=90
 movieDuration=[1,10,25,35,60]
-
-
+movieDuration=[1, 10, 25, 35, 60, 40, 20, 10, 15, 60]
 
 def foo(flightDuration, movieDuration):
     best=0
@@ -21,15 +20,18 @@ def foo(flightDuration, movieDuration):
                         result = xi, yi
     return result
 
+# does not work if several movies have the same length
 def foo2(flightDuration, movieDuration):
-    movies_dic = {v: k for k, v in list(enumerate(movieDuration))}
+    movies_dic = {v: k for k, v in enumerate(movieDuration)}
     for _len in sorted(movies_dic, reverse=True):
-        print(_len)
         remaining_len = flightDuration-30-_len
         if remaining_len in movies_dic:
             return movies_dic[remaining_len], movies_dic[_len]
     return -1,-1
-foo2(90, [1,10,25,35,60])
+#foo2(90, [1,10,25,35,60])
+
+import timeit
+print(timeit.timeit('foo(90, [1, 10, 25, 35, 60, 40, 20, 10, 15, 60])', globals=globals()))
 
 """
 QUESTION 2
