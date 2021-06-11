ab50d2ce3737d456f78486531f2fc9bb3d6fb0c7
KCLaurelie <aurelie.mascio@kcl.ac.uk>
2021-06-11 18:00:47 +0100

    +1

--- brainteasers/stacks_and_all.py
+++ brainteasers/stacks_and_all.py
@@ -66,3 +66,34 @@ qobj.deQueue(queue=q)
 
 #endregion
 
+#region Largest Rectangle
+# https://www.hackerrank.com/challenges/largest-rectangle/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=stacks-queues
+# https://www.geeksforgeeks.org/largest-rectangle-under-histogram/
+# Runtime: O(n)
+
+def getMaxArea(arr):
+    s = [-1]
+    n = len(arr)
+    area = 0
+    i = 0
+    left_smaller = [-1] * n
+    right_smaller = [n] * n
+    while i < n:
+        while s and (s[-1] != -1) and (arr[s[-1]] > arr[i]):
+            right_smaller[s[-1]] = i
+            s.pop()
+        if ((i > 0) and (arr[i] == arr[i - 1])):
+            left_smaller[i] = left_smaller[i - 1]
+        else:
+            left_smaller[i] = s[-1]
+        s.append(i)
+        i += 1
+    for j in range(0, n):
+        area = max(area, arr[j] * (right_smaller[j] - left_smaller[j] - 1))
+    return area
+
+
+hist = [6, 2, 5, 4, 5, 1, 6]
+print("maxArea = ", getMaxArea(hist))
+#endregion
+
